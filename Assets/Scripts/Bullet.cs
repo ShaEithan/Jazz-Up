a519617bfbad7a99330442fd8c60d5c77649a3ef
M4TT4 <okamimatta@yahoo.com>
2024-07-13 18:01:23 -0500

    new shit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+     // Start is called before the first frame update
+     public float speed;
+     private Rigidbody2D rb;
+    
+
+    // Update is called once per frame
+    private void Update()
+    {     
+          
+          transform.position += transform.right * Time.deltaTime * speed;
+    }
+
+     private void OnCollisionEnter2D(Collision2D collision)
+     {
+          Destroy(gameObject);
+     }
+}
